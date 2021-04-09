d1f0c9a372b0e470c48e00816ab5669246a9d197
AlexanderStewartCollege <stew0554@algonquinlive.com>
2021-04-08 23:31:19 -0400

    setting up to push to Heroku

--- client/src/constants.js
+++ client/src/constants.js
@@ -3,7 +3,7 @@ let API = null;
 if (process.env.NODE_ENV === "development") {
   API = "http://localhost:3001";
 } else if (process.env.NODE_ENV === "production") {
-  API = "https://cig-quit.herokuapp.com";
+  API = "https://cigquit.herokuapp.com";
 }
 
 export { API as API_HOST };
